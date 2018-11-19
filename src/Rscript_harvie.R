693a4661d48bb8c7ac801c2426dd4c69eb060b49
Cory Cascalheira <17078341+corycaaz@users.noreply.github.com>
2018-11-19 15:52:38 -0500

    Descriptive statistics and specify assumptions

--- src/Rscript_harvie.R
+++ src/Rscript_harvie.R
@@ -1,5 +1,18 @@
 ####### SETUP WORKSPACE #######
 
+library(tidyverse)
+library(psych)
+library(car)
 
+# Set working directory
+setwd("~/GitHub/osl-harvie-et-al-2015/src")
 
-####### CLEAN DATA #######
+# Import dataset
+harvie <- read_csv("../data/Harvie et al. 2015.csv")
+
+####### DESCRIPTIVE STATISTICS #######
+
+(harvie_desc <- harvie %>%
+  select(ends_with("Feedback")) %>%
+  describe()
+)
